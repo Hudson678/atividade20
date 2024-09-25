f26d52ae428337106a2f416e7a886d82cdc63101
Hudson678 <Hudsonmores31@gmail.com>
2024-09-25 13:55:56 -0300

    numeros de pares de 1 a 100

---
+++ paresimpares1a100.py
@@ -0,0 +1,9 @@
+# Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
+
+s = 0
+for n in range (1,101):
+    if n % 2 == 0:
+       s += n
+print (s) 
+        
+
